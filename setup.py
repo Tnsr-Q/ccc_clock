abce0b1e588d7d4f3b591563d7c69fa8a1be7557
Tnsr-Q <quantquiplabs@gmail.com>
2025-09-04 11:58:11 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ccc_clock",
+    version="0.1.0",
+    packages=find_packages(),
+    description="Your project description here",
+    author="Your Name",
+    author_email="your@email.com",
+)
